374ad4c93afc0efae2217a15c21a97e45666332c
Treybrowder <tbrowder7@gmail.com>
2024-01-18 20:33:58 -0500

    Initial project set up

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Trey Browder on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
